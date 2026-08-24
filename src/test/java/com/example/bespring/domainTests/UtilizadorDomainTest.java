483dd248bf6659ffbbb20062f1dca96de9737c47
RitaCJ <ritajoao0121@gmail.com>
2026-08-24 22:41:47 +0100

    test: adiciona teste unitário (Não deve criar utilizador com primeiro nome ou sobrenome vázio)

--- src/test/java/com/example/bespring/domainTests/UtilizadorDomainTest.java
+++ src/test/java/com/example/bespring/domainTests/UtilizadorDomainTest.java
@@ -7,6 +7,7 @@ import com.example.bespring.domain.enums.Perfil;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UtilizadorDomainTest {
 
@@ -20,13 +21,45 @@ public class UtilizadorDomainTest {
 
         Escola escola = new Escola("School", "Rua francisco da silva", "9234234445", "school@gmail.com");
 
-        Utilizador utilizador = new Utilizador(primeiroNome, sobrenome, genero, perfil);
+        Utilizador utilizador = new Utilizador(primeiroNome, sobrenome, genero, perfil, escola);
 
         assertEquals(primeiroNome, utilizador.getPrimeiroNome());
         assertEquals(sobrenome, utilizador.getSobrenome());
         assertEquals(genero, utilizador.getGenero());
         assertEquals(perfil, utilizador.getPerfil());
+        assertEquals(escola, utilizador.getEscola());
 
+    }
+
+    @Test
+    void naoDeveCriarUmUtilizadorComPrimeiroNomeVazio() throws Exception {
+
+        String primeiroNome = "";
+        String sobrenome = "Zuri";
+        Genero genero = Genero.FEMININO;
+        Perfil perfil = Perfil.ALUNO;
+
+        Escola escola = new Escola("School", "Rua francisco da silva", "9234234445", "school@gmail.com");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+           new Utilizador(primeiroNome, sobrenome, genero, perfil, escola);
+        });
+
+    }
+
+    @Test
+    void naoDeveCriarUmUtilizadorComSobrenomeVazio() throws Exception {
+
+        String primeiroNome = "Ana";
+        String sobrenome = "";
+        Genero genero = Genero.FEMININO;
+        Perfil perfil = Perfil.ALUNO;
+
+        Escola escola = new Escola("School", "Rua francisco da silva", "9234234445", "school@gmail.com");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+           new Utilizador(primeiroNome, sobrenome, genero, perfil, escola);
+        });
 
     }
 
