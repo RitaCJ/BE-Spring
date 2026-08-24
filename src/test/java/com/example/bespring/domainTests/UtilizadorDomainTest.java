4a0b49f9651b8ddeb04500454889d18d520e7e46
RitaCJ <ritajoao0121@gmail.com>
2026-08-24 13:50:28 +0100

    test: adiciona teste unitario (criar utilizador com campos validos)

--- src/test/java/com/example/bespring/domainTests/UtilizadorDomainTest.java
+++ src/test/java/com/example/bespring/domainTests/UtilizadorDomainTest.java
@@ -1,4 +1,33 @@
 package com.example.bespring.domainTests;
 
+import com.example.bespring.domain.Escola;
+import com.example.bespring.domain.Utilizador;
+import com.example.bespring.domain.enums.Genero;
+import com.example.bespring.domain.enums.Perfil;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UtilizadorDomainTest {
+
+    @Test
+    void deveCriarUmUtilizadorComCamposValidos() {
+
+        String primeiroNome = "Asa";
+        String sobrenome = "Zuri";
+        Genero genero = Genero.FEMININO;
+        Perfil perfil = Perfil.ALUNO;
+
+        Escola escola = new Escola("School", "Rua francisco da silva", "9234234445", "school@gmail.com");
+
+        Utilizador utilizador = new Utilizador(primeiroNome, sobrenome, genero, perfil);
+
+        assertEquals(primeiroNome, utilizador.getPrimeiroNome());
+        assertEquals(sobrenome, utilizador.getSobrenome());
+        assertEquals(genero, utilizador.getGenero());
+        assertEquals(perfil, utilizador.getPerfil());
+
+
+    }
+
 }
