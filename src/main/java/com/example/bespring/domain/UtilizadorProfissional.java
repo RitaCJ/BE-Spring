4ad1187339e816e4aca9c829b7a71d61991fc541
RitaCJ <ritajoao0121@gmail.com>
2026-08-28 11:14:30 +0100

    Adiciona novos teste unitários e novas funcionalidades  para utilizador profissional.

--- src/main/java/com/example/bespring/domain/UtilizadorProfissional.java
+++ src/main/java/com/example/bespring/domain/UtilizadorProfissional.java
@@ -39,16 +39,30 @@ public class UtilizadorProfissional extends Utilizador{
 
     public UtilizadorProfissional(String primeiroNome, String sobrenome, String telefone, Genero genero, String email, String senha, TipoProfissional tipo, Perfil perfil, Escola escola){
 
+        this.senha = senha;
+
         if(primeiroNome == null || primeiroNome.isBlank()){
-            throw new IllegalArgumentException("O campo primeiro nome não pode estar vazio");
+            throw new IllegalArgumentException("O campo primeiro nome não pode estar vazio.");
         }else if(sobrenome == null || sobrenome.isBlank()){
-            throw new IllegalArgumentException("O campo sobrenome não pode estar vazio");
+            throw new IllegalArgumentException("O campo sobrenome não pode estar vazio.");
         }else if(telefone == null || telefone.isBlank()){
-            throw new IllegalArgumentException("O campo telefone não pode estar vazio");
+            throw new IllegalArgumentException("O campo telefone não pode estar vazio.");
         }else if(email == null || email.isBlank()){
-            throw new IllegalArgumentException("O campo email não pode estar vazio");
+            throw new IllegalArgumentException("O campo email não pode estar vazio.");
         }else if(senha == null || senha.isBlank()){
-            throw new IllegalArgumentException("O campo senha não pode estar vazio");
+            throw new IllegalArgumentException("O campo senha não pode estar vazio.");
+        }else if(senha.length() < 8){
+            throw new IllegalArgumentException("A senha é muito curta.");
+        }else if(senha.length() > 30){
+            throw new IllegalArgumentException("A senha é muito longa.");
+        }else if(!temMaiuscula()){
+            throw new IllegalArgumentException("A senha não tem letra maíuscula.");
+        }else if(!temMinuscula()){
+            throw new IllegalArgumentException("A senha não tem letra minuscula.");
+        }else if(!temNumero()){
+            throw new IllegalArgumentException("A senha não tem numero.");
+        }else if(!temCaracterEspecial()){
+            throw new IllegalArgumentException("A senha não tem caractere especial.");
         }
 
         this.setPrimeiroNome(primeiroNome);
@@ -56,10 +70,49 @@ public class UtilizadorProfissional extends Utilizador{
         this.telefone = telefone;
         this.setGenero(genero);
         this.email = email;
-        this.senha = senha;
         this.tipo = tipo;
         this.setPerfil(perfil);
         this.setEscola(escola);
     }
 
+    private boolean temMaiuscula(){
+
+        for(char c : this.senha.toCharArray()){
+            if(Character.isUpperCase(c)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean temMinuscula(){
+
+        for(char c : senha.toCharArray()){
+            if(Character.isLowerCase(c)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean temNumero(){
+
+        for(char c : senha.toCharArray()){
+            if(Character.isDigit(c)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean temCaracterEspecial(){
+        String especiais = "@#$%!";
+        for(char c : senha.toCharArray()){
+            if(especiais.indexOf(c) >= 0){
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/test/java/com/example/bespring/domainTests/UtilizadorProfissionalDomainTest.java
+++ src/test/java/com/example/bespring/domainTests/UtilizadorProfissionalDomainTest.java
@@ -8,8 +8,7 @@ import com.example.bespring.domain.enums.Perfil;
 import com.example.bespring.domain.enums.TipoProfissional;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UtilizadorProfissionalDomainTest {
 
@@ -21,7 +20,7 @@ public class UtilizadorProfissionalDomainTest {
         String telefone = "933434445";
         Genero genero = Genero.FEMININO;
         String email = "ana@gmail.com";
-        String senha = "123456";
+        String senha = "Feliznatal2026%";
         TipoProfissional tipo = TipoProfissional.PROFESSOR;
         Perfil perfil = Perfil.ALUNO;
 
@@ -132,5 +131,118 @@ public class UtilizadorProfissionalDomainTest {
         assertThrows(IllegalArgumentException.class, () -> {
             new UtilizadorProfissional(primeiroNome, sobrenome, telefone, genero, email, senha, tipo, perfil, escola);
         });
+    }
+
+    @Test
+    void naoDeveCriarUtilizadorProfissionalComSenhaCurta() throws Exception{
+
+        String primeiroNome = "Ana";
+        String sobrenome = "Bell";
+        String telefone = "933434445";
+        Genero genero = Genero.FEMININO;
+        String email = "ana@gmail.com";
+        String senha = "asdf354";
+        TipoProfissional tipo = TipoProfissional.PROFESSOR;
+        Perfil perfil = Perfil.ALUNO;
+
+        Escola escola = new Escola("School", "Rua francisco da silva", "9234234445", "school@gmail.com");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new UtilizadorProfissional(primeiroNome, sobrenome, telefone, genero, email, senha, tipo, perfil, escola);
+        });
+    }
+
+    @Test
+    void naoDeveCriarUtilizadorProfissonalComSenhaMuitoLonga() throws Exception{
+
+        String primeiroNome = "Ana";
+        String sobrenome = "Bell";
+        String telefone = "933434445";
+        Genero genero = Genero.FEMININO;
+        String email = "ana@gmail.com";
+        String senha = "asdf3523kkkdjjdfsdfghfghfvfgdff";
+        TipoProfissional tipo = TipoProfissional.PROFESSOR;
+        Perfil perfil = Perfil.ALUNO;
+
+        Escola escola = new Escola("School", "Rua francisco da silva", "9234234445", "school@gmail.com");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new UtilizadorProfissional(primeiroNome, sobrenome, telefone, genero, email, senha, tipo, perfil, escola);
+        });
+
+    }
+
+    @Test
+    void naoDeveCriarUtilizadorProfissionalComSenhaSemLetraMaiuscula() throws Exception{
+
+        String primeiroNome = "Ana";
+        String sobrenome = "Bell";
+        String telefone = "933434445";
+        Genero genero = Genero.FEMININO;
+        String email = "ana@gmail.com";
+        String senha = "avante";
+        TipoProfissional tipo = TipoProfissional.PROFESSOR;
+        Perfil perfil = Perfil.ALUNO;
+
+        Escola escola = new Escola("School", "Rua francisco da silva", "9234234445", "school@gmail.com");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new UtilizadorProfissional(primeiroNome, sobrenome, telefone, genero, email, senha, tipo, perfil, escola);
+        });
+    }
+
+    @Test
+    void naoDeveCriarUtilizadorProfissionalComSenhaSemLetraMinuscula() throws Exception{
+        String primeiroNome = "Ana";
+        String sobrenome = "Bell";
+        String telefone = "933434445";
+        Genero genero = Genero.FEMININO;
+        String email = "ana@gmail.com";
+        String senha = "AVANTE";
+        TipoProfissional tipo = TipoProfissional.PROFESSOR;
+        Perfil perfil = Perfil.ALUNO;
+
+        Escola escola = new Escola("School", "Rua francisco da silva", "9234234445", "school@gmail.com");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new UtilizadorProfissional(primeiroNome, sobrenome, telefone, genero, email, senha, tipo, perfil, escola);
+        });
+    }
+
+    @Test
+    void naoDeveCriarUtilizadorProfissionaComSenhaSemNumero() throws Exception{
+        String primeiroNome = "Ana";
+        String sobrenome = "Bell";
+        String telefone = "933434445";
+        Genero genero = Genero.FEMININO;
+        String email = "ana@gmail.com";
+        String senha = "Avante";
+        TipoProfissional tipo = TipoProfissional.PROFESSOR;
+        Perfil perfil = Perfil.ALUNO;
+
+        Escola escola = new Escola("School", "Rua francisco da silva", "9234234445", "school@gmail.com");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new UtilizadorProfissional(primeiroNome, sobrenome, telefone, genero, email, senha, tipo, perfil, escola);
+        });
+    }
+
+    @Test
+    void naoDeveCriarUtilizadorPrissionalComSenhaSemCaracteresEspeciais() throws Exception{
+        String primeiroNome = "Ana";
+        String sobrenome = "Bell";
+        String telefone = "933434445";
+        Genero genero = Genero.FEMININO;
+        String email = "ana@gmail.com";
+        String senha = "@Avante";
+        TipoProfissional tipo = TipoProfissional.PROFESSOR;
+        Perfil perfil = Perfil.ALUNO;
+
+        Escola escola = new Escola("School", "Rua francisco da silva", "9234234445", "school@gmail.com");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new UtilizadorProfissional(primeiroNome, sobrenome, telefone, genero, email, senha, tipo, perfil, escola);
+        });
+
     }
 }
