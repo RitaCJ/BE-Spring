59db58189f32a057d7dd99449ae0ab9d57022430
RitaCJ <ritajoao0121@gmail.com>
2026-09-05 22:30:32 +0100

    feat: Adiciona serviço de autorização.

--- src/main/java/com/example/bespring/services/AuthorizationService.java
+++ src/main/java/com/example/bespring/services/AuthorizationService.java
@@ -1,4 +1,32 @@
 package com.example.bespring.services;
 
-public class AuthorizationService {
+import com.example.bespring.repository.UtilizadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+
+    private final UtilizadorRepository utilizadorRepository;
+
+    public AuthorizationService(UtilizadorRepository utilizadorRepository) {
+        this.utilizadorRepository = utilizadorRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("Usuario recebido: " + username);
+        //Consultar os utilizadores no banco de dados.
+        //return utilizadorRepository.findByLogin(username);
+        UserDetails user = utilizadorRepository.findByLogin(username);
+
+        System.out.println("Usuario encontrado: " + user.getUsername());
+
+        return user;
+    }
 }
