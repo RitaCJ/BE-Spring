7356b69a0dca28a601ea4545ff4787e95aa5bfd5
RitaCJ <ritajoao0121@gmail.com>
2026-09-05 22:26:34 +0100

    feat: Repositorio do utilizador.

--- src/main/java/com/example/bespring/repository/UtilizadorRepository.java
+++ src/main/java/com/example/bespring/repository/UtilizadorRepository.java
@@ -1,4 +1,10 @@
 package com.example.bespring.repository;
 
-public interface UtilizadorRepository {
+import com.example.bespring.domain.Utilizador;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UtilizadorRepository extends JpaRepository<Utilizador, Long> {
+    //Metódo sem implementação.
+    UserDetails findByLogin(String login);
 }
