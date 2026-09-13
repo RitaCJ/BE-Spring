4244c36124e271c4177447d8579d57a6e3add334
RitaCJ <ritajoao0121@gmail.com>
2026-09-13 22:34:55 +0100

    Atualiza8

--- src/main/java/com/example/bespring/dto/CriarAlunoRequest.java
+++ src/main/java/com/example/bespring/dto/CriarAlunoRequest.java
@@ -1,4 +1,17 @@
 package com.example.bespring.dto;
 
-public record CriarAlunoRequest() {
-}
+import com.example.bespring.domain.enums.Genero;
+import com.example.bespring.domain.enums.TipoProfissional;
+
+public record CriarAlunoRequest(
+        String primeiroNome,
+        String sobrenome,
+        String nomeUtilizador,
+        int numeroAluno,
+        String sala,
+        String corFavorita,
+        boolean possuiDaltonismo,
+        Genero genero,
+        String senha,
+        Long idEscola
+) { }
