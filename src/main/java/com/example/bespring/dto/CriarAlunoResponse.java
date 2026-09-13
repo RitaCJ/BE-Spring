55856a24fb12f49837553dc8441ed7cbc5a13563
RitaCJ <ritajoao0121@gmail.com>
2026-09-13 22:35:05 +0100

    Atualiza9

--- src/main/java/com/example/bespring/dto/CriarAlunoResponse.java
+++ src/main/java/com/example/bespring/dto/CriarAlunoResponse.java
@@ -1,4 +1,20 @@
 package com.example.bespring.dto;
 
-public record CriarAlunoResponse() {
-}
+import com.example.bespring.domain.enums.Genero;
+import com.example.bespring.domain.enums.Perfil;
+
+public record CriarAlunoResponse(
+        long idUtilizador,
+        String primeiroNome,
+        String sobrenome,
+        String nomeUtilizador,
+        int numeroAluno,
+        String sala,
+        String corFavorita,
+        boolean possuiDaltonismo,
+        Genero genero,
+        Perfil perfil,
+        Long idEscola,
+        Long idTurma,
+        Long idProfessor
+) { }
